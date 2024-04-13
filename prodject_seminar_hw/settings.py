b224559418d1cfd2a8e1bba81a9702a9bc268897
Andrey <avgust1@mail.ru>
2024-04-13 23:06:51 +0500

    Выгрузка сайта20252

--- prodject_seminar_hw/settings.py
+++ prodject_seminar_hw/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-#SECRET_KEY = 'django-insecure-dm@sf!_c_&n^eq7ck7$5x54)01^^b5b^^q6^ap&8zne@6r-pb5'
+#SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-dm@sf!_c_&n^eq7ck7$5x54)01^^b5b^^q6^ap&8zne@6r-pb5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 SESSION_COOKIE_SECURE = True
